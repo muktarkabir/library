3fd202d43c33ab511a1d7f3ff4346cfeddd9d737
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-03-02 00:47:40 +0100

    Style header

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const toggleThemeBtn = document.querySelector("#toggle-theme");
 
+console.log(toggleThemeBtn);
 
 // Setiing up default light mode
 setLightTheme();
@@ -9,6 +10,8 @@ toggleThemeBtn.addEventListener("click", function () {
   const currentTheme = document.documentElement.getAttribute("data-theme");
   const newTheme = currentTheme === "dark" ? "light" : "dark";
   document.documentElement.setAttribute("data-theme", newTheme);
+  this.classList.toggle("fa-moon");
+  this.classList.add("fa-sun");
 });
 
 function setLightTheme() {
@@ -16,7 +19,5 @@ function setLightTheme() {
 }
 
 function setDarkTheme() {
-    document.documentElement.setAttribute("data-theme", "dark");
-  }
-
-
+  document.documentElement.setAttribute("data-theme", "dark");
+}
